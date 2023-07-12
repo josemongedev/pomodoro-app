a24e47a95eb0f00e9aa0f61858cda35987cdf16f
Jose Monge <info@josemonge.dev>
2023-07-12 16:38:04 -0600

    Added alarm and click sounds to timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import Timer from "./components/Timer";
 
 function App() {
   return <Timer />;
+  {
+    /* Sound Effect from{" "}
+    <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=104243">
+      Pixabay
+    </a> */
+  }
 }
 
 export default App;
--- src/components/TimerDisplay.tsx
+++ src/components/TimerDisplay.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useTimerControls } from "../hooks/useTimerControls";
 import RadialProgressbar from "./RadialProgressbar";
 import { TColorOptions } from "../types/timerTypes";
@@ -23,6 +23,26 @@ const TimerDisplay = React.memo(({ minutesInterval, accentColor }: Props) => {
   const hidePauseButton = () => (idle || pause ? "hidden" : "");
   const hideRestartButton = () => (!idle ? "hidden" : "");
 
+  const buttonClickedRef = useRef<HTMLAudioElement>(null);
+  const alarmCompleteRef = useRef<HTMLAudioElement>(null);
+
+  const onButtonClicked = (onTimerHandler: () => void) => {
+    buttonClickedRef.current
+      ?.play()
+      .then(() => {
+        onTimerHandler();
+      })
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    const playTimerComplete = async () => {
+      if (idle && minutes === 0 && seconds === 0)
+        await alarmCompleteRef.current?.play();
+    };
+    playTimerComplete().catch(console.error);
+  }, [idle, minutes, seconds]);
+
   return (
     <section className="w-[300px] tablet:w-[410px] aspect-square rounded-full bg-blueblack grid place-items-center shadow-lg">
       <div className="w-full bg-gradient-to-br from-black to-blue rounded-full grid place-items-center p-[22px]">
@@ -32,23 +52,33 @@ const TimerDisplay = React.memo(({ minutesInterval, accentColor }: Props) => {
           </span>
           <div className="w-full flex justify-center row-start-5 relative z-10">
             <button
-              onClick={onTimerStart}
+              onClick={() => onButtonClicked(onTimerStart)}
               className={`uppercase hover:text-${accentColor} font-bold text-md-sp tablet:text-xl-sp ${hideStartButton()}`}
             >
               &nbsp;start
             </button>
             <button
-              onClick={onTimerPause}
+              onClick={() => onButtonClicked(onTimerPause)}
               className={`uppercase hover:text-${accentColor} font-bold text-md-sp tablet:text-xl-sp ${hidePauseButton()}`}
             >
               &nbsp;pause
             </button>
             <button
-              onClick={onTimerRestart}
+              onClick={() => onButtonClicked(onTimerRestart)}
               className={`uppercase hover:text-${accentColor} font-bold text-md-sp tablet:text-xl-sp ${hideRestartButton()}`}
             >
               &nbsp;restart
             </button>
+            <audio
+              ref={buttonClickedRef}
+              src="/pomodoro-app/audio/button-clicked.mp3"
+              hidden
+            ></audio>
+            <audio
+              ref={alarmCompleteRef}
+              src="/pomodoro-app/audio/alarm-complete.mp3"
+              hidden
+            ></audio>
           </div>
           <RadialProgressbar
             minutes={minutes}
