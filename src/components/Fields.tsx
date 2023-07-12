cf4fb70ebfa57cba3d6c4858ec0e586d36a63ac0
Jose Monge <info@josemonge.dev>
2023-07-12 08:05:19 -0600

    Replaced the div for a legend tag inside the fielset container in the Fields component (Fields.tsx) Added float-left class to make legend responsive to flexbox (required)

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -6,13 +6,11 @@ type Props = {
 
 const Fields = ({ legend, children }: PropsWithChildren<Props>) => {
   return (
-    <fieldset className="w-full  tablet:flex tablet:justify-between tablet:items-center tablet:flex-wrap">
-      <div className="w-full tablet:w-auto uppercase text-xxs-sp tablet:text-sm-sp font-bold text-center  leading-none">
+    <fieldset className="w-full flex flex-col tablet:flex-row tablet:justify-between tablet:items-center tablet:flex-wrap gap-[18px] tablet:gap-0">
+      <legend className="float-left w-full tablet:w-auto uppercase text-xxs-sp tablet:text-sm-sp font-bold text-center  leading-none">
         {legend}
-      </div>
-      <div className="mt-[18px] tablet:mt-[0px] flex justify-center gap-4 ">
-        {children}
-      </div>
+      </legend>
+      <div className="flex justify-center gap-4 ">{children}</div>
     </fieldset>
   );
 };
